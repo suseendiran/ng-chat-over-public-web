709088f6ed2bd280caab8ed87e28d36377fdb018
susee <suseecheetah@gmail.com>
2021-03-28 17:57:51 +0530

    initial commit

---
+++ apps/chat/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-chat-over-public-web-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'chat';
+}
